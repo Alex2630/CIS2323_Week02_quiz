583fc4d060945cf380a960d4aa8d0bca40724e27
Alex2630 <42626222+Alex2630@users.noreply.github.com>
2018-08-29 18:25:56 -0500

    Cap string

--- Message.java
+++ Message.java
@@ -1,6 +1,6 @@
 public class Message{
 	
-	public static void main (string[] args){
+	public static void main (String[] args){
 	system.out.println("Alexander Martin");
 	system.out.println("How to properly use the command promt and run files kinda java");
 	}
