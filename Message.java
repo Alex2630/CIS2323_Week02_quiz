0b3abcf6685d27c416893b9857d86083f59e8c2c
Alex2630 <42626222+Alex2630@users.noreply.github.com>
2018-08-29 18:20:49 -0500

    semi colon

--- Message.java
+++ Message.java
@@ -1,7 +1,7 @@
 public class Message{
 	
 	public static void main (string[] args){
-	system.out.println("Alexander Martin")
+	system.out.println("Alexander Martin");
 	system.out.println("How to properly use the command promt and run files kinda java");
 	}
 }
